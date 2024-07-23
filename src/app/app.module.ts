ca9c901df9c4929ca94a5c44236ff13a3dd5e7f1
Marko Baba <markobaba22@gmail.com>
2024-07-23 14:00:22 +0200

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScenarioDetailsComponent } from './scenario-details/scenario-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ScenarioDetailsComponent } from './scenario-details/scenario-details.co
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
   ],
   providers: [
     provideClientHydration(),
